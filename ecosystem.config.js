47649df6622c68a618d4ae9d190a0a1b612d79e2
Илья Гирсенок <igirsenok@nota.tech>
2024-07-01 23:12:07 +0300

    правки

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/themison/nodejs-mesto-backend.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
